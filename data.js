965efb3c0082b4b32ce059bc9579898ab83222a6
Osama <osama22@gmail.com>
2026-09-21 23:09:38 +0300

    small edit data

--- data.js
+++ data.js
@@ -220,8 +220,8 @@ export const DEMO_PACKAGES = [
 ];
 
 export const PLATFORM_STATS = [
-  { id: 'stat-1', value: 28, prefix: '+', suffix: '', num: '+28', progress: 88, growthAr: '+18% شهرياً', growthEn: '+18% MoM', labelEn: 'Padel & Football Courts', labelAr: 'ملعب بادل وخماسي' },
-  { id: 'stat-2', value: 5, prefix: '+', suffix: 'K', num: '+5K', progress: 92, growthAr: '+24% أسبوعياً', growthEn: '+24% WoW', labelEn: 'Active Local Players', labelAr: 'لاعب نشط بالصعيد' },
+  { id: 'stat-1', value: 28, prefix: '+', suffix: '', num: '+28', progress: 88, growthAr: '+18% شهرياً', growthEn: '+18% MoM', labelEn: 'Padel & Football Courts', labelAr: 'ملعب  ' },
+  { id: 'stat-2', value: 5, prefix: '+', suffix: 'K', num: '+5K', progress: 92, growthAr: '+24% أسبوعياً', growthEn: '+24% WoW', labelEn: 'Active Local Players', labelAr: 'لاعب  ' },
   { id: 'stat-3', value: 99, prefix: '', suffix: '%', num: '99%', progress: 99, growthAr: 'دقة 100%', growthEn: '100% On-time', labelEn: 'Time Slot Accuracy', labelAr: 'دقة المواعيد بالملاعب' },
   { id: 'stat-4', value: 12, prefix: '+', suffix: 'K', num: '+12K', progress: 95, growthAr: '+35% بطولات', growthEn: '+35% Tournaments', labelEn: 'Hours Booked', labelAr: 'ساعة محجوزة بالصعيد' },
 ];
